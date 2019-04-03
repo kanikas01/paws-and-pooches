0ea2603cf16bba0dab34f025dfa1c35e8e254c68
kanikas01 <larvakurt@yahoo.com>
2019-04-02 17:32:50 -0700

    Add file for user api routes and require it in server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
+require("./routes/userApiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
