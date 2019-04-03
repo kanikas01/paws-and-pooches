7859b50f2719bc8fc2234a6f59b46ed23091394e
kanikas01 <larvakurt@yahoo.com>
2019-04-03 09:08:26 -0700

    Add page specific JS, break user and pet functionality into separate files

--- public/js/user.js
+++ public/js/user.js
@@ -1,104 +1,105 @@
-// Get references to page elements
-var $userName = $("#username");
-var $userEmail = $("#email");
-var $submitBtn = $("#submit");
-var $userList = $("#user-list");
-
-// Initialize Materialize components
+// Wait until page is loaded
 $(document).ready(function() {
+  // Get references to page elements
+  var $userName = $("#username");
+  var $userEmail = $("#email");
+  var $submitBtn = $("#submit");
+  var $userList = $("#user-list");
+
+  // Initialize Materialize components
   $("select").formSelect();
-});
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveUser: function(user) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/users",
-      data: JSON.stringify(user)
-    });
-  },
-  getUsers: function() {
-    return $.ajax({
-      url: "api/users",
-      type: "GET"
-    });
-  },
-  deleteUser: function(id) {
-    return $.ajax({
-      url: "api/users/" + id,
-      type: "DELETE"
-    });
-  }
-};
-
-// refreshUsers gets new users from the db and repopulates the list
-var refreshUsers = function() {
-  API.getUsers().then(function(data) {
-    var $users = data.map(function(user) {
-      var $a = $("<a>")
-        .text(user.text)
-        .attr("href", "/user/" + user.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": user.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $userList.empty();
-    $userList.append($users);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new user
-// Save the new user to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var user = {
-    name: $userName.val().trim(),
-    email: $userEmail.val().trim()
+  // The API object contains methods for each kind of request we'll make
+  var API = {
+    saveUser: function(user) {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/users",
+        data: JSON.stringify(user)
+      });
+    },
+    getUsers: function() {
+      return $.ajax({
+        url: "api/users",
+        type: "GET"
+      });
+    },
+    deleteUser: function(id) {
+      return $.ajax({
+        url: "api/users/" + id,
+        type: "DELETE"
+      });
+    }
   };
 
-  if (!(user.name && user.email)) {
-    alert("You must enter a user name and email!");
-    return;
-  }
+  // refreshUsers gets new users from the db and repopulates the list
+  var refreshUsers = function() {
+    API.getUsers().then(function(data) {
+      var $users = data.map(function(user) {
+        var $a = $("<a>")
+          .text(user.text)
+          .attr("href", "/user/" + user.id);
 
-  API.saveUser(user).then(function() {
-    refreshUsers();
-  });
+        var $li = $("<li>")
+          .attr({
+            class: "list-group-item",
+            "data-id": user.id
+          })
+          .append($a);
 
-  $userName.val("");
-  $userEmail.val("");
-};
+        var $button = $("<button>")
+          .addClass("btn btn-danger float-right delete")
+          .text("ｘ");
 
-// handleDeleteBtnClick is called when a user's delete button is clicked
-// Remove the user from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
+        $li.append($button);
 
-  API.deleteUser(idToDelete).then(function() {
-    refreshUsers();
-  });
-};
+        return $li;
+      });
 
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$userList.on("click", ".delete", handleDeleteBtnClick);
+      $userList.empty();
+      $userList.append($users);
+    });
+  };
+
+  // handleFormSubmit is called whenever we submit a new user
+  // Save the new user to the db and refresh the list
+  var handleFormSubmit = function(event) {
+    event.preventDefault();
+
+    var user = {
+      name: $userName.val().trim(),
+      email: $userEmail.val().trim()
+    };
+
+    if (!(user.name && user.email)) {
+      alert("You must enter a user name and email!");
+      return;
+    }
+
+    API.saveUser(user).then(function() {
+      refreshUsers();
+    });
+
+    $userName.val("");
+    $userEmail.val("");
+  };
+
+  // handleDeleteBtnClick is called when a user's delete button is clicked
+  // Remove the user from the db and refresh the list
+  var handleDeleteBtnClick = function() {
+    var idToDelete = $(this)
+      .parent()
+      .attr("data-id");
+
+    API.deleteUser(idToDelete).then(function() {
+      refreshUsers();
+    });
+  };
+
+  // Add event listeners to the submit and delete buttons
+  $submitBtn.on("click", handleFormSubmit);
+  $userList.on("click", ".delete", handleDeleteBtnClick);
+});
