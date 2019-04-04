06725a9dccd8ef133807493492c10b7ac056665a
kanikas01 <larvakurt@yahoo.com>
2019-04-04 16:42:00 -0700

    Add compression package to reduce page load times

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var compression = require("compression");
 
 var db = require("./models");
 
@@ -8,6 +9,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(compression());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
