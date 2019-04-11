886d4bdac3f5ec96867dea4cc3b65c251bc29839
kanikas01 <larvakurt@yahoo.com>
2019-04-10 22:34:44 -0700

    Remove get all users functionality

--- routes/userApiRoutes.js
+++ routes/userApiRoutes.js
@@ -1,13 +1,6 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all users
-  // app.get("/api/users", function(req, res) {
-  //   db.User.findAll({}).then(function(dbUser) {
-  //     res.json(dbUser);
-  //   });
-  // });
-
   // Create a new user
   app.post("/api/users", function(req, res) {
     db.User.create(req.body)
